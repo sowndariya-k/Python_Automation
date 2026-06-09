78d7358ce59921afd94dbf555dd5df1cc00d3664
sowndariya <sowndariyadeveloper@gmail.com>
2026-06-09 21:08:24 +0530

    logger

--- Pytest/DataDriven_excel/Utilities/excelReader.py
+++ Pytest/DataDriven_excel/Utilities/excelReader.py
@@ -1,16 +1,14 @@
 import openpyxl
-
-def get_data(path, sheet_name):
+def get_data(path,sheet_name):
     final_list=[]
     workbook=openpyxl.load_workbook(path)
     sheet=workbook[sheet_name]
     total_row=sheet.max_row
     total_col=sheet.max_column
-
-    for r in range(2, total_row+1):
+    
+    for r in range(2,total_row+1):
         row_list=[]
-        for c in range(1, total_col+1):
-            row_list.append(sheet.cell(row=r, column=c).value)
+        for c in range(1,total_col+1):
+            row_list.append(sheet.cell(r,c).value)
         final_list.append(row_list)
-
     return final_list
